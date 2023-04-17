5ac552414deac4baccbae3921058eebef3e6ab92
MiguelGaticaBestNickname123 <mi.gatica@duocuc.cl>
2023-04-17 10:34:45 -0400

    Cositas editadas
    El estilo, editar precio y fondo de pumblus

--- js/script-index.js
+++ js/script-index.js
@@ -2,6 +2,6 @@
 function showConsequences() {
   var consequencesWindow = window.open("", "Consequences", "width=400, height=530");
   var consequencesImage = document.createElement("img");
-  consequencesImage.src = "E:/PWEB/Proyecto/Pumblus0/PlumbusBeta/imgs/PM-187.webp";
+  consequencesImage.src = "D:/Pumblus/PlumbusBeta/imgs/PM-187.webp";
   consequencesWindow.document.body.appendChild(consequencesImage);
 }
