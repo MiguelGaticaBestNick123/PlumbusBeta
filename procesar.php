b21cf36d01b1e000a292f93f1e5297c9753363d2
yoelpipeflay <fe.galan@duocuc.cl>
2023-05-08 03:16:30 -0400

    listo el formulario de crear usuario

---
+++ procesar.php
@@ -0,0 +1,11 @@
+<?php
+//procesar.php
+
+//Aquí debes agregar la validación de los datos enviados desde el formulario
+
+if (/*aquí debes agregar la condición para validar los datos*/) {
+  //Todo está correcto, redireccionamos al usuario a la página "index.html"
+header('Location: index.html');
+exit();
+}
+?>
